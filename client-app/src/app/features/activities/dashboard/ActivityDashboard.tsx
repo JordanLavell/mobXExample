ce2ea7c958deb1765942f6adf29f5aa0ad3b7d13
Jordan <jordanlavell@googlemail.com>
2022-03-21 23:14:01 +0000

    agent file for Axios

--- client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/app/features/activities/dashboard/ActivityDashboard.tsx
@@ -14,15 +14,21 @@ interface Props {
   openForm: (id: string) => void;
   closeForm: () => void;
   createOrEdit: (activity: Activity) => void;
-  deleteActivity: (id: string) => void
+  deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
 
-export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity }: Props) {
+export default function ActivityDashboard({ activities, selectedActivity, selectActivity, cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity, submitting }: Props) {
   return (
     <Grid>
       <Grid.Column width='10'>
         <List>
-          <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity} />
+          <ActivityList
+            activities={activities}
+            selectActivity={selectActivity}
+            deleteActivity={deleteActivity}
+            submitting={submitting}
+          />
         </List>
       </Grid.Column>
       <GridColumn width='6'>
@@ -33,7 +39,7 @@ export default function ActivityDashboard({ activities, selectedActivity, select
             openForm={openForm}
           />
         }
-        {editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+        {editMode && <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} submitting={submitting} />}
       </GridColumn>
     </Grid>
   );
