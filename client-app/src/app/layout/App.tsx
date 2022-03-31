9690c7691cc90d27f3ab8556d67b2c1f87710d8b
Jordan Lavell <jordanlavell@googlemail.com>
2022-04-01 00:19:29 +0100

    Error Handling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -10,24 +10,30 @@ import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import TestErrors from "../../features/errors/TestError";
 import { ToastContainer } from "react-toastify";
 import NotFound from "../../features/errors/NotFound";
+import ServerError from "../../features/errors/ServerError";
 
 function App() {
   const location = useLocation();
   return (
     <>
       <ToastContainer position="bottom-right" hideProgressBar />
-      <Route exact path='/' component={HomePage} />
+      <Route exact path="/" component={HomePage} />
       <Route
-        path={'/(.+)'}
+        path={"/(.+)"}
         render={() => (
           <>
             <NavBar />
             <Container style={{ marginTop: "7em" }}>
               <Switch>
-                <Route exact path='/activities' component={ActivityDashboard} />
-                <Route path='/activities/:id' component={ActivityDetails} />
-                <Route key={location.key} path={['/createActivity', '/manage/:id']} component={ActivityForm} />
-                <Route path={'/errors'} component={TestErrors} />
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={["/createActivity", "/manage/:id"]}
+                  component={ActivityForm}
+                />
+                <Route path={"/errors"} component={TestErrors} />
+                <Route path={"/server-error"} component={ServerError} />
                 <Route component={NotFound} />
               </Switch>
             </Container>
